5907732f04561daf912c734a9933e7e639592286
garciagenrique <enrique.garcia.garcia@cern.ch>
2023-11-20 18:13:22 +0100

    triggers new image rucio noise

---
+++ containers/rucio-noise/init_rucio.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+# Authors:
+# - Vincent Garonne, <vgaronne@gmail.com>, 2018
+
+## Customised version to make v32.0.0 run on the VRE - DEV cluster
+
+shopt -s checkwinsize
+
+if [ ! -f /opt/rucio/etc/rucio.cfg ]; then
+    echo "File rucio.cfg not found. It will generate one."
+    mkdir -p /opt/rucio/etc/
+#    python3 /usr/local/rucio_client/merge_rucio_configs.py \
+#        -s /opt/user/rucio.default.cfg \
+    python3 /usr/local/rucio/tools/merge_rucio_configs.py \
+        --use-env \
+        -d /opt/rucio/etc/rucio.cfg
+fi
+
+echo "Enable shell completion on the rucio commands"
+eval "$(register-python-argcomplete rucio)"
+eval "$(register-python-argcomplete rucio-admin)"
